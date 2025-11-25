92ccfba9a2465184e5bbfbee2ac403f63e799a7e
darabul <darabulin@yandex.ru>
2025-11-26 02:54:10 +0300

    calcu

--- calculator.py
+++ calculator.py
@@ -1,9 +1,1 @@
-a = float(input("Первое число: "))
-b = float(input("Второе число: "))
-operation = input("Операция (+, -, *, /): ")
-
-if operation == "+": print(a + b)
-elif operation == "-": print(a - b)
-elif operation == "*": print(a * b)
-elif operation == "/": print(a / b if b != 0 else "Ошибка!")
-else: print("Неизвестная операция")# could you calculate this
+change in calculator
